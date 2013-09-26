afdb39ac1a4e2c0a73f657a908be16efdc427f24
Harrison Brown <harrisonbro@gmail.com>
2013-09-26 17:36:54 +0100

    Added local root definition to local config file

--- local-config-sample.php
+++ local-config-sample.php
@@ -10,3 +10,5 @@ define( 'DB_NAME', 'local_db_name' );
 define( 'DB_USER', 'local_db_user' );
 define( 'DB_PASSWORD', 'local_db_password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+defined('SITE_ROOT') ? null : define('SITE_ROOT', 'local_site_root'); // eg. '/Users/...'
