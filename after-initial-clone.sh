e41f76bc8900f7503552e4127fef83e95a997211
Harrison Brown <harrisonbro@gmail.com>
2013-10-09 15:16:51 +0100

    Added shell file to de-submodule the submodules
    Ie. Brings them all into normal tracking with git

---
+++ after-initial-clone.sh
@@ -0,0 +1,23 @@
+# Removed cached history of submodules
+git rm --cached wp
+git rm --cached content/themes/wp-skeleton-theme
+git rm --cached content/plugins/wordpress-seo
+git rm --cached content/plugins/w3-total-cache
+
+# Remove submodules
+git rm .gitmodules
+
+# Remove .git folders in submodules
+rm -rf wp/.git
+rm -rf content/themes/wp-skeleton-theme/.git
+rm -rf content/plugins/wordpress-seo/.git
+rm -rf content/plugins/w3-total-cache/.git
+
+# Re-add submodule folders
+git add wp
+git add content/themes/wp-skeleton-theme
+git add content/plugins/wordpress-seo
+git add content/plugins/w3-total-cache
+
+# Commit
+git commit -m "De-submoduled submodules"
